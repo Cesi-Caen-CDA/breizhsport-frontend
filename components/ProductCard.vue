d1ab3e7924fc4ea81b69dd177f9784d45a56db96
Rhayonne Lopes <rhayonne@gmail.com>
2025-01-26 21:23:38 +0100

    List all products

---
+++ components/ProductCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="product-card">
+    <img :src="product.image" :alt="product.title" class="product-image" />
+    <div class="product-info">
+      <h3>{{ product.title }}</h3>
+      <p>{{ product.description }}</p>
+      <span>Categoria: {{ product.category }}</span>
+      <div class="price">Preço: ${{ product.price }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({ product: { type: Object, required: true } });
+</script>
+
+<style></style>
