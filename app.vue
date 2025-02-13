4588d0f1ce801d08bef4be50771a7798e96e6a67
Rhayonne Lopes <rhayonne@gmail.com>
2025-02-13 01:33:16 +0100

    Correction Layout

--- app.vue
+++ app.vue
@@ -1,20 +1,13 @@
 <template>
-  <v-app>
-    <!-- Barra lateral -->
-    <Sidebar />
-
-    <!-- Área principal do conteúdo -->
-    <v-main>
-      <!-- A página será renderizada aqui -->
-      <NuxtPage />
-    </v-main>
-  </v-app>
+  <NuxtLayout>
+    <v-app>
+      <v-main>
+        <NuxtPage />
+      </v-main>
+    </v-app>
+  </NuxtLayout>
 </template>
 
-<script setup>
-import Sidebar from "@/components/Sidebar.vue"; // Certifique-se de que o caminho do Sidebar está correto
-</script>
+<script setup></script>
 
-<style scoped>
-/* Estilos globais, se necessário */
-</style>
+<style scoped></style>
