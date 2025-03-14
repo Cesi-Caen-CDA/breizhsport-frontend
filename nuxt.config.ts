29e0d5b1f0f3036110bb6f0b210df998c7d85e08
Bouisr <r.bouis14@gmail.com>
2025-03-14 15:10:36 +0100

    Page : S'inscrire, Se connecter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   components: true,
-  modules: [
-    'vuetify-nuxt-module',
-    '@pinia/nuxt',
-    '@nuxtjs/google-fonts',
-    '@nuxt/icon',
-  ],
-  build:{
-    transpile: ['@pinia/nuxt']
+  modules: ["vuetify-nuxt-module", "@pinia/nuxt", "@nuxtjs/google-fonts", "@nuxt/icon", "@pinia/nuxt"],
+  build: {
+    transpile: ["@pinia/nuxt"],
   },
   googleFonts: {
     families: {
-      'Poppins': ['400', '500', '700'],
-      'Montserrat': ['400', '500', '600', '700'],
+      Poppins: ["400", "500", "700"],
+      Montserrat: ["400", "500", "600", "700"],
     },
-  display: 'swap',
-},
-app:{
-  head:{
-    link:[
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/icon?family=Material+Icons",
-      
-      }
-    ]
-  }
-}
-})
+    display: "swap",
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
+});
